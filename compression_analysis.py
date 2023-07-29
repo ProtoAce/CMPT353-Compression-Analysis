c8675562273e9beba49f34be7586ca8cac46ab84
Misha <mishagoldenberg@gmail.com>
2023-07-28 17:39:17 -0700

    best compression type predictor

--- compression_analysis.py
+++ compression_analysis.py
@@ -12,6 +12,7 @@ import time
 import matplotlib.pyplot as plt
 import pandas as pd
 
+compression_types = ['zlib', 'gzip', 'bz2', 'lzma', 'zipfile', 'tarfile']
 
 def train_models():
     data = pd.read_csv('compression_times.csv')
@@ -21,8 +22,7 @@ def train_models():
     data['single_file'] = le.fit_transform(data['single_file'])
 
     X = data[['file_size', 'compression_type', 'single_file']]
-    # y = data[['compression_time', 'compression_ratio']]
-    y = data['compression_time']
+    y = data[['compression_time', 'compression_ratio']]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
@@ -43,5 +43,38 @@ def train_models():
     print("mlp train:", model_mlp.score(X_train, y_train))
     print("mlp test:", model_mlp.score(X_test, y_test))
 
+    return model_kneighbors, model_random_forest, model_mlp
 
-train_models()
+model_kneighbors, model_random_forest, model_mlp = train_models()
+
+def predict_compression_alg(filetype, filesize, model_type){
+    model = None
+    if model_type == 'kneighbors':
+        model = model_kneighbors
+    elif model_type == 'random_forest':
+        model = model_random_forest
+    else:
+        model = model_mlp
+
+    min_compression_time = float("inf")
+    min_compression_time_type = None
+    max_compression_ratio = float("-inf")
+    max_compression_ratio_type = None
+
+    for compression_type in compression_types:
+        single_file = 0
+        X = [[filesize, compression_type, single_file]]
+        y = model.predict(X)
+        if y[0] < min_compression_time:
+            min_compression_time = y[0]
+            min_compression_time_type = compression_type
+        if y[1] > max_compression_ratio:
+            max_compression_ratio = y[1]
+            max_compression_ratio_type = compression_type  
+        
+    return min_compression_time_type, max_compression_ratio_type
+}
+
+
+
+
