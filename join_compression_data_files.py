3b29f86f16f6af77cc2cbe60f610fe6de998a046
Misha <mishagoldenberg@gmail.com>
2023-08-04 16:14:23 -0700

    final compression times file + image

--- join_compression_data_files.py
+++ join_compression_data_files.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
-compression_data_NVMe = pd.read_csv('compression_times/compression_times_GCheng_NVMe.csv')
-compression_data_HDD = pd.read_csv('compression_times/compression_times_HDD.csv')
-compression_data_SSD = pd.read_csv('compression_times/compression_times_GCheng_SSD.csv')
+compression_data_NVMe = pd.read_csv(
+    'compression_times/compression_times_GCheng_NVMe.csv')
+compression_data_HDD = pd.read_csv(
+    'compression_times/compression_times_HDD.csv')
+compression_data_SSD = pd.read_csv(
+    'compression_times/compression_times_GCheng_SSD.csv')
 
-joined_data = pd.concat([compression_data_NVMe, compression_data_HDD, compression_data_SSD])
+joined_data = pd.concat(
+    [compression_data_NVMe, compression_data_HDD, compression_data_SSD])
+joined_data.loc[(joined_data['file_type'] != 'json') & (
+    joined_data['file_type'] != 'txt') & (joined_data['file_type'] != 'csv'), 'file_type'] = 'file'
 joined_data.to_csv('compression_times/compression_times.csv', index=False)
