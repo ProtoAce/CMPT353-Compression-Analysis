b8778795174071c1c26fe21ed65de25f2963df06
gyc5 <gyc5@sfu.ca>
2023-08-03 17:35:36 -0700

    all created files in typed locations

--- compression_functions.py
+++ compression_functions.py
@@ -4,7 +4,7 @@ import bz2
 import lzma
 import zipfile
 import tarfile
-
+import os
 
 # Compress data using zlib
 def zlib_compress(data):
@@ -47,18 +47,17 @@ def lzma_decompress(data):
     return decompressed_data
 
 # Compress data using zipfile
-def zip_compress(data_path):
+def zip_compress(data_path): # data_path = file_path
     with zipfile.ZipFile('data.zip', 'w', zipfile.ZIP_DEFLATED) as compressed_data:
-        compressed_data.write(data_path)
-    with zipfile.ZipFile('compressed.zip', 'a', zipfile.ZIP_DEFLATED) as compressed_data:
         compressed_data.write(data_path)
     with open('data.zip', 'rb') as compressed_data:
         compressed_data = compressed_data.read()
     return compressed_data
 # Decompress data using zipfile
-def zip_decompress(data):
-    decompressed_data = zipfile.decompress(data)
-    return decompressed_data
+def zip_decompress(out_file):
+    with zipfile.ZipFile(out_file, 'r') as f:
+         f.extractall("data/data/compressed_files/")  
+         return
 
 # Compress data using tarfile
 def tar_compress(data_path):
@@ -69,9 +68,10 @@ def tar_compress(data_path):
     return compressed_data
 
 # Decompress data using tarfile
-def tar_decompress(data):
-    decompressed_data = tarfile.decompress(data)
-    return decompressed_data
+def tar_decompress(out_file):
+    with tarfile.open(out_file, 'r') as f:
+        f.extractall("data/data/compressed_files/")
+        return
 
 
 
