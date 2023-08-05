d20dd0658bd7dbcee09cc8138a52865dcac4e69c
Misha <mishagoldenberg@gmail.com>
2023-08-04 18:50:50 -0700

    modified decompression anova

--- decompression_analysis_anova.py
+++ decompression_analysis_anova.py
@@ -3,57 +3,53 @@ from statsmodels.stats.multicomp import pairwise_tukeyhsd
 import pandas as pd
 
 
-def decompression_time_anova(data, storage_type):
-    x1 = data.loc[(data['compression_type'] == 'zlib') & (
-        data['storage_type'] == storage_type), 'decompression_time']
-    x2 = data.loc[(data['compression_type'] == 'gzip') & (
-        data['storage_type'] == storage_type), 'decompression_time']
-    x3 = data.loc[(data['compression_type'] == 'bz2') & (
-        data['storage_type'] == storage_type), 'decompression_time']
-    x4 = data.loc[(data['compression_type'] == 'lzma') & (
-        data['storage_type'] == storage_type), 'decompression_time']
-    x5 = data.loc[(data['compression_type'] == 'zipfile') & (
-        data['storage_type'] == storage_type), 'decompression_time']
-    x6 = data.loc[(data['compression_type'] == 'tarfile') & (
-        data['storage_type'] == storage_type), 'decompression_time']
+def decompression_time_anova(data):
+    x1 = data.loc[(data['compression_type'] == 'zlib'), 'decompression_time']
+    x2 = data.loc[(data['compression_type'] == 'gzip'), 'decompression_time']
+    x3 = data.loc[(data['compression_type'] == 'bz2'), 'decompression_time']
+    x4 = data.loc[(data['compression_type'] == 'lzma'), 'decompression_time']
+    x5 = data.loc[(data['compression_type'] == 'zipfile'),
+                  'decompression_time']
+    x6 = data.loc[(data['compression_type'] == 'tarfile'),
+                  'decompression_time']
 
     anova = stats.f_oneway(x1, x2, x3, x4, x5, x6)
     return anova.pvalue
 
 
-def decompression_time_posthoc(data, storage_type):
-    x1 = data.loc[(data['compression_type'] == 'zlib') & (
-        data['storage_type'] == storage_type), 'decompression_time'].reset_index(drop=True)
-    x2 = data.loc[(data['compression_type'] == 'gzip') & (
-        data['storage_type'] == storage_type), 'decompression_time'].reset_index(drop=True)
-    x3 = data.loc[(data['compression_type'] == 'bz2') & (
-        data['storage_type'] == storage_type), 'decompression_time'].reset_index(drop=True)
-    x4 = data.loc[(data['compression_type'] == 'lzma') & (
-        data['storage_type'] == storage_type), 'decompression_time'].reset_index(drop=True)
-    x5 = data.loc[(data['compression_type'] == 'zipfile') & (
-        data['storage_type'] == storage_type), 'decompression_time'].reset_index(drop=True)
-    x6 = data.loc[(data['compression_type'] == 'tarfile') & (
-        data['storage_type'] == storage_type), 'decompression_time'].reset_index(drop=True)
+def decompression_time_posthoc(data):
+    x1 = data.loc[(data['compression_type'] == 'zlib'),
+                  'decompression_time'].reset_index(drop=True)
+    x2 = data.loc[(data['compression_type'] == 'gzip'),
+                  'decompression_time'].reset_index(drop=True)
+    x3 = data.loc[(data['compression_type'] == 'bz2'),
+                  'decompression_time'].reset_index(drop=True)
+    x4 = data.loc[(data['compression_type'] == 'lzma'),
+                  'decompression_time'].reset_index(drop=True)
+    x5 = data.loc[(data['compression_type'] == 'zipfile'),
+                  'decompression_time'].reset_index(drop=True)
+    x6 = data.loc[(data['compression_type'] == 'tarfile'),
+                  'decompression_time'].reset_index(drop=True)
     df = pd.DataFrame({'zlib': x1, 'gzip': x2, 'bz2': x3,
                       'lzma': x4, 'zipfile': x5, 'tarfile': x6})
     x_melt = pd.melt(df)
     posthoc = pairwise_tukeyhsd(
-        x_melt['value'], x_melt['variable'], alpha=0.05)
+        x_melt['value'], x_melt['variable'], alpha=(0.05)/3)
     print(posthoc)
+    fig = posthoc.plot_simultaneous()
+    fig.show()
+    fig.savefig('decompression_speed.png')
 
 
 def anova_analyis():
     data = pd.read_csv('compression_times.csv')
-    storage_types = ['HDD', 'SSD', 'NVMe']
-    for storage_type in storage_types:
-        print('storage_type: ', storage_type)
-        decompression_time_pvalue = decompression_time_anova(
-            data, storage_type)
-        print('decompression_time_pvalue: ', decompression_time_pvalue)
+    decompression_time_pvalue = decompression_time_anova(
+        data)
+    print('decompression_time_pvalue: ', decompression_time_pvalue)
 
-        if decompression_time_pvalue < 0.05:
-            print('decompression_time_posthoc')
-            decompression_time_posthoc(data, storage_type)
+    if decompression_time_pvalue < (0.05)/3:
+        print('decompression_time_posthoc')
+        decompression_time_posthoc(data)
 
 
 def main():
